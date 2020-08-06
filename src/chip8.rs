50da050b25bc855bd2ba9b74242c849f2eb2c278
Matthew Dibble <mdibble@rogers.com>
2020-08-05 20:02:31 -0400

    Added more opcodes

--- src/chip8.rs
+++ src/chip8.rs
@@ -64,15 +64,19 @@ impl CHIP8 {
         print!("Executing 0x{:x}{:x}{:x}{:x}... ", op1, op2, op3, op4);
         
         match (op1, op2, op3, op4) {
-            (0x0, 0x0, 0xe, 0xe) => self.op_00ee(),
+            (0x0, 0x0, 0xE, 0x0) => self.op_00e0(),
+            (0x0, 0x0, 0xE, 0xE) => self.op_00ee(),
             (0x1, _, _, _) => self.op_1nnn(),
             (0x2, _, _, _) => self.op_2nnn(),
             (0x3, _, _, _) => self.op_3xnn(op2),
             (0x4, _, _, _) => self.op_4xnn(op2),
+            (0x5, _, _, 0x0) => self.op_5xy0(op2, op3),
             (0x6, _, _, _) => self.op_6xnn(op2),
             (0x7, _, _, _) => self.op_7xnn(op2),
             (0x8, _, _, 0x0) => self.op_8xy0(op2, op3),
+            (0x8, _, _, 0x1) => self.op_8xy1(op2, op3),
             (0x8, _, _, 0x2) => self.op_8xy2(op2, op3),
+            (0x8, _, _, 0x3) => self.op_8xy3(op2, op3),
             (0x8, _, _, 0x4) => self.op_8xy4(op2, op3),
             (0x8, _, _, 0x5) => self.op_8xy5(op2, op3),
             (0xA, _, _, _) => self.op_annn(),
@@ -89,6 +93,14 @@ impl CHIP8 {
         }
 
         println!("success!");
+    }
+
+    // 00E0: Clears the screen
+    fn op_00e0(&mut self) {
+        for i in 0..self.gfx.len() {
+            self.gfx[i] = 0;
+        }
+        self.pc = self.opcode & 0x0FFF;
     }
 
     // 00EE: Returns from a subroutine
@@ -130,6 +142,16 @@ impl CHIP8 {
         }
     }
 
+    // 5XY0: Skips the next instruction if VX equals VY
+    fn op_5xy0(&mut self, x: u8, y: u8) {
+        if self.v[x as usize] == self.v[y as usize] as u8 {
+            self.pc += 4;
+        }
+        else {
+            self.pc += 2;
+        }
+    }
+
     // 6XNN: Sets VX to NN
     fn op_6xnn(&mut self, x: u8) {
         self.v[x as usize] = (self.opcode & 0x00FF) as u8;
@@ -148,9 +170,21 @@ impl CHIP8 {
         self.pc += 2;
     }
 
+    // 8XY1: Sets VX to VX or VY (bitwise OR operation)
+    fn op_8xy1(&mut self, x: u8, y: u8) {
+        self.v[x as usize] = self.v[x as usize] | self.v[y as usize];
+        self.pc += 2;
+    }
+
     // 8XY2: Sets VX to VX and VY (bitwise AND operation)
     fn op_8xy2(&mut self, x: u8, y: u8) {
         self.v[x as usize] = self.v[x as usize] & self.v[y as usize];
+        self.pc += 2;
+    }
+
+    // 8XY3: Sets VX to VX xor VY (bitwise xor operation)
+    fn op_8xy3(&mut self, x: u8, y: u8) {
+        self.v[x as usize] = self.v[x as usize] ^ self.v[y as usize];
         self.pc += 2;
     }
 
@@ -180,10 +214,25 @@ impl CHIP8 {
         self.pc += 2;
     }
 
+    // 9XY0: Skips the next instruction if VX doesn't equal VY
+    fn op_9xy0(&mut self, x: u8, y: u8) {
+        if self.v[x as usize] != self.v[y as usize] {
+            self.pc += 4;
+        }
+        else {
+            self.pc += 2;
+        }
+    }
+
     // ANNN: Sets I to the address NNN
     fn op_annn(&mut self) {
         self.i = (self.opcode & 0x0FFF) as u16;
         self.pc += 2;
+    }
+
+    // BNNN: Jumps to the address NNN plus V0
+    fn op_bnnn(&mut self) {
+        self.pc = (self.opcode & 0x0FFF) as u16 + self.v[0] as u16;
     }
 
     // CXNN: Sets VX to the result of a bitwise and operation on a random number and NN
@@ -228,6 +277,16 @@ impl CHIP8 {
         self.pc += 2;
     }
 
+    // EX9E: Skips the next instruction if the key stored in VX is pressed
+    fn op_ex9e(&mut self, x: u8) {
+        if self.key[self.v[x as usize] as usize] == true {
+            self.pc += 4;
+        }
+        else {
+            self.pc += 2;
+        }
+    }
+
     // EXA1: Skips the next instruction if the key stored in VX isn't pressed
     fn op_exa1(&mut self, x: u8) {
         if self.key[self.v[x as usize] as usize] == false {
@@ -256,6 +315,12 @@ impl CHIP8 {
         self.pc += 2;
     }
 
+    // FX1E: Adds VX to I, VF is not affected
+    fn op_fx1e(&mut self, x: u8) {
+        self.i += self.v[x as usize] as u16;
+        self.pc += 2;
+    }
+
     // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
     fn op_fx29(&mut self, x: u8) {
         self.i = self.v[x as usize] as u16 * 5;
@@ -268,6 +333,14 @@ impl CHIP8 {
         self.memory[self.i as usize] = val / 100; 
         self.memory[(self.i + 1) as usize] = (val / 10) % 10; 
         self.memory[(self.i + 2) as usize] = val % 10; 
+        self.pc += 2;
+    }
+
+    // Dumps V0 to VX inclusive in memory starting at address I. The offset from I is increased by 1 for each value written
+    fn op_fx55(&mut self, x: u8) {
+        for index in 0..=x as usize {
+            self.memory[self.i as usize + index] = self.v[index];
+        }
         self.pc += 2;
     }
 
